2ce51c91d77bc42b5eb6434c96ecd07bbba45b5e
Prabhat <mmrprabhujii@gmail.com>
2025-07-11 11:22:57 +0530

    rfcer

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+ 
+import Navbar from "./Componentes/Navbar/Navbar";
+import Main from "./Componentes/Main/Main"
+import Cards from "./Componentes/Cards/Cards"
+import Categories from "./Componentes/Categories/Categories"
+import Footer from "./Componentes/Footer/Footer"
+
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      <Main/>
+      <Cards/>
+      <Categories />
+      <Footer/>
+
+    </>
+  );
+}
